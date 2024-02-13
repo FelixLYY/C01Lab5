acec9e3f3797daa5d27ce01f3c447d094ffaeb90
Felix Lee <felix.yy.lee@gmail.com>
2024-02-13 00:12:18 -0500

    Modify gitignore and Finish getAllNotes 0 Note test

--- quirknotes/backend/tests/status200.test.js
+++ quirknotes/backend/tests/status200.test.js
@@ -22,46 +22,55 @@ test("/postNote - Post a note", async () => {
 });
 
 test("/getAllNotes - Return list of zero notes for getAllNotes", async () => {
-    // Code here
-    expect(false).toBe(true);
+  const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    });
+
+    const getAllNotesBody = await getAllNotesRes.json();
+    
+    expect(getAllNotesRes.status).toBe(200);
+    expect(getAllNotesBody.response.length).toBe(0);
   });
   
-  test("/getAllNotes - Return list of two notes for getAllNotes", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
+test("/getAllNotes - Return list of two notes for getAllNotes", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
   
-  test("/deleteNote - Delete a note", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
+test("/deleteNote - Delete a note", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
   
-  test("/patchNote - Patch with content and title", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
+test("/patchNote - Patch with content and title", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
   
-  test("/patchNote - Patch with just title", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
+test("/patchNote - Patch with just title", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
   
-  test("/patchNote - Patch with just content", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
+test("/patchNote - Patch with just content", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
   
-  test("/deleteAllNotes - Delete one note", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
+test("/deleteAllNotes - Delete one note", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
   
-  test("/deleteAllNotes - Delete three notes", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
+test("/deleteAllNotes - Delete three notes", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
   
-  test("/updateNoteColor - Update color of a note to red (#FF0000)", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
+test("/updateNoteColor - Update color of a note to red (#FF0000)", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
